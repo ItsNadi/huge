328cb681f700d8e8e1a8bae829606edeb4fc23ed
ItsNadi <ninaunicornkeks@gmail.com>
2026-05-13 08:28:34 +0200

    Update index.php

--- application/view/admin/index.php
+++ application/view/admin/index.php
@@ -3,55 +3,81 @@
 
     <div class="box">
 
-        <!-- echo out the system feedback (error and success messages) -->
         <?php $this->renderFeedbackMessages(); ?>
-
-        <h3>What happens here ?</h3>
 
         <div>
             This controller/action/view shows a list of all users in the system. with the ability to soft delete a user
             or suspend a user.
         </div>
-        <div>
-            <table class="overview-table">
-                <thead>
-                <tr>
-                    <td>Id</td>
-                    <td>Avatar</td>
-                    <td>Username</td>
-                    <td>User's email</td>
-                    <td>Activated ?</td>
-                    <td>Link to user's profile</td>
-                    <td>suspension Time in days</td>
-                    <td>Soft delete</td>
-                    <td>Submit</td>
-                </tr>
-                </thead>
-                <?php foreach ($this->users as $user) { ?>
+
+        <table class="overview-table">
+            <thead>
+            <tr>
+                <td>Id</td>
+                <td>Avatar</td>
+                <td>Username</td>
+                <td>User's email</td>
+                <td>Activated ?</td>
+                <td>Group</td>
+                <td>Link to user's profile</td>
+                <td>suspension Time in days</td>
+                <td>Soft delete</td>
+                <td>Group Change</td>
+                <td>Submit</td>
+            </tr>
+            </thead>
+
+            <?php foreach ($this->users as $user) { ?>
+                <form action="<?= Config::get("URL"); ?>admin/actionAccountSettings" method="post">
                     <tr class="<?= ($user->user_active == 0 ? 'inactive' : 'active'); ?>">
+
                         <td><?= $user->user_id; ?></td>
+
                         <td class="avatar">
                             <?php if (isset($user->user_avatar_link)) { ?>
                                 <img src="<?= $user->user_avatar_link; ?>"/>
                             <?php } ?>
                         </td>
+
                         <td><?= $user->user_name; ?></td>
                         <td><?= $user->user_email; ?></td>
                         <td><?= ($user->user_active == 0 ? 'No' : 'Yes'); ?></td>
+
+                        <td><?= $user->group_name ?? 'No group'; ?></td>
+
                         <td>
                             <a href="<?= Config::get('URL') . 'profile/showProfile/' . $user->user_id; ?>">Profile</a>
                         </td>
-                        <form action="<?= config::get("URL"); ?>admin/actionAccountSettings" method="post">
-                            <td><input type="number" name="suspension" /></td>
-                            <td><input type="checkbox" name="softDelete" <?php if ($user->user_deleted) { ?> checked <?php } ?> /></td>
-                            <td>
-                                <input type="hidden" name="user_id" value="<?= $user->user_id; ?>" />
-                                <input type="submit" />
-                            </td>
-                        </form>
+
+                        <td>
+                            <input type="number" name="suspension" />
+                        </td>
+
+                        <td>
+                            <input type="checkbox" name="softDelete"
+                                    <?php if ($user->user_deleted) { ?> checked <?php } ?> />
+                        </td>
+
+                        <td>
+                            <select name="group_id">
+                                <?php foreach ($this->groups as $group) { ?>
+                                    <option value="<?= $group->id ?>"
+                                            <?= ($group->id == $user->user_group_id) ? 'selected' : '' ?>>
+                                        <?= $group->name ?>
+                                    </option>
+                                <?php } ?>
+                            </select>
+                        </td>
+
+                        <td>
+                            <input type="hidden" name="user_id" value="<?= $user->user_id; ?>" />
+                            <input type="submit" />
+                        </td>
+
                     </tr>
-                <?php } ?>
-            </table>
-        </div>
+                </form>
+            <?php } ?>
+        </table>
+
     </div>
 </div>
